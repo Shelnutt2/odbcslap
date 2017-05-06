484f3055d5ab24592351fa62e7be221869a615e0
Seth Shelnutt <Shelnutt2@gmail.com>
2017-05-06 18:07:05 -0400

    Add env db variable to sqlite setup. #3

--- test/ci_setup/sqlite_setup.sh
+++ test/ci_setup/sqlite_setup.sh
@@ -3,6 +3,8 @@
 # Licensed under MIT per nanodb project.
 # Taken from https://github.com/lexicalunit/nanodbc/blob/master/utility/ci/travis/before_script.sqlite.sh
 
+export TEST_DSN="DRIVER=SQLite3;Database=test.db"
+
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     DRIVER="$(brew ls -v sqliteodbc | grep libsqlite3odbc.dylib)"
     cat >"$(odbc_config --odbcinstini)" <<EOF
