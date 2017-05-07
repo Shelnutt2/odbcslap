f8508ab5a9f15e36c76aae13ee520d84765e2db3
Seth Shelnutt <Shelnutt2@gmail.com>
2017-05-07 08:40:10 -0400

    Set passwords for dsn password test. #3

--- test/ci_setup/postgresql_setup.sh
+++ test/ci_setup/postgresql_setup.sh
@@ -2,7 +2,8 @@
 
 sudo odbcinst -i -d -f /usr/share/psqlodbc/odbcinst.ini.template
 psql -c "CREATE DATABASE test;" -U postgres
+psql -c "create USER odbcslap WITH PASSWORD 'odbcslap';" -U postgres
 export ODBCSLAP_TEST_DSN="DRIVER={PostgreSQL ANSI};Server=localhost;Port=5432;Database=test;UID=postgres;"
-export ODBCSLAP_TEST_DSN_NO_PW="DRIVER={PostgreSQL ANSI};Server=localhost;Port=5432;Database=test"
-export ODBCSLAP_TEST_USERNAME="postgres"
-export ODBCSLAP_TEST_PASSWORD=""
+export ODBCSLAP_TEST_DSN_NO_PW="DRIVER={PostgreSQL ANSI};Server=localhost;Port=5432;Database=test;"
+export ODBCSLAP_TEST_USERNAME="odbcslap"
+export ODBCSLAP_TEST_PASSWORD="odbcslap"
